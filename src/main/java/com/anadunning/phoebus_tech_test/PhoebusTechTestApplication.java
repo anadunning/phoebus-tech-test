b5f009599bb83fbbc50c66d558068405db7855af
anadunning <anakpedrosa@hotmail.com>
2025-07-22 18:50:29 -0300

    First commit - Created project

---
+++ src/main/java/com/anadunning/phoebus_tech_test/PhoebusTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.anadunning.phoebus_tech_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoebusTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoebusTechTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anadunning/phoebus_tech_test/PhoebusTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anadunning.phoebus_tech_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoebusTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
